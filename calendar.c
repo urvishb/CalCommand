ebbd984cafcab8a7220d83299ec24de8d098d9a5
Urvish <ubundela55@gmail.com>
2020-10-24 18:37:15 +0530

    Part 1 implemented it can imitate the Cal command

--- calendar.c
+++ calendar.c
@@ -1,6 +1,11 @@
+// These changes are from collaborator's side
+//the second time i am changing something as a contributor 24th oct
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
+int is_leap(int yr);
 
 int main()
 {
@@ -32,10 +37,10 @@ int main()
     {
         for (int i = 1900 ; i < year ; i++)
         {
-            if((i % 400 == 0) || ((i % 4 == 0) && (i % 100 != 0)))
+            if(is_leap(i) == 1)
             {
                 leap_count++;
-            }  
+            }
         }
     }
 
@@ -43,7 +48,7 @@ int main()
     {
         for (int j = year ; j <= 1900 ; j++)
         { 
-            if((j % 400 == 0) || ((j % 4 == 0) && (j % 100 != 0)))
+            if(is_leap(j) == 1)
             {
                 leap_count++;
             }
@@ -54,9 +59,80 @@ int main()
 
     int total_days = ((leap_count * 366) + (Normal_years) * 365 );
 
+    int yrpoint = 0;
+
+    if(year >= 1900)
+    {
+        while(strcmpi(allMonths[yrpoint], month) != 0)
+        {
+            if(yrpoint == 0 || yrpoint == 2 || yrpoint == 4 || yrpoint == 6 || yrpoint == 7 || yrpoint == 9 || yrpoint == 11)
+            {
+                    //printf("\n\nThis month had/has/will have 31 days\n\n");
+                    total_days += 31;
+            }
+
+            else if (yrpoint == 3 || yrpoint == 5 || yrpoint == 8 || yrpoint == 10)
+            {
+                //printf("\n\nThis month had/has/will have 30 days\n\n");
+                total_days += 30;
+            }
+
+            else
+            {
+                if (is_leap(year) == 1)
+                {
+                    //printf("\nThis month had/has/will have 29 days\n\n");
+                    total_days += 29;
+                }
+
+                else
+                {
+                    //printf("\nThis month had/has/will have 28 days\n\n");
+                    total_days += 28;
+                }
+            }
+            yrpoint ++;      
+        }
+    }
+
+    else
+    {
+        while(strcmpi(allMonths[yrpoint], month) != 0)
+        {
+            if(yrpoint == 0 || yrpoint == 2 || yrpoint == 4 || yrpoint == 6 || yrpoint == 7 || yrpoint == 9 || yrpoint == 11)
+            {
+                    //printf("\n\nThis month had/has/will have 31 days\n\n");
+                    total_days -= 31;
+            }
+
+            else if (yrpoint == 3 || yrpoint == 5 || yrpoint == 8 || yrpoint == 10)
+            {
+                //printf("\n\nThis month had/has/will have 30 days\n\n");
+                total_days -= 30;
+            }
+
+            else
+            {
+                if (is_leap(year) == 1)
+                {
+                    //printf("\nThis month had/has/will have 29 days\n\n");
+                    total_days -= 29;
+                }
+
+                else
+                {
+                    //printf("\nThis month had/has/will have 28 days\n\n");
+                    total_days -= 28;
+                }
+            }
+            yrpoint ++;      
+        }
+    }
+    
+
     int week_day;
 
-    if(year == 1900)
+    if(year == 1900 && (strcmpi(allMonths[0], month) == 0))
     {
         week_day = 0;
     } 
@@ -67,27 +143,33 @@ int main()
     }
     
 
-    if(year > 1900)
+    if(year >= 1900)
     {
         week_day = week_day + 0;
     }
 
-    else
+    else if (year < 1900)
     {
         week_day = 7 - week_day; 
     }
 
-    char days[7][10] = {"Monday",
-                         "Tuesday",
-                         "Wednesday",
-                         "Thursday",
-                         "Friday" ,
-                         "Saturday",
-                         "Sunday"}; 
+    // else
+    // {
+    //     week_day = 0;
+    // }
+    
 
-    printf ("\n\nThe day at date - 01/ Jan / %d was %s!\n\n", year, days[week_day]);
+    char days[7][10] = {"Mon",
+                         "Tue",
+                         "Wed",
+                         "Thu",
+                         "Fri" ,
+                         "Sat",
+                         "Sun"}; 
 
+    // printf ("\n\nThe day at date - 01 / %s / %d was/is/will be %s!\n\n", month, year, days[week_day]);
 
+    int days_in_mnth;
 
     for(currm = 0 ; currm < 12 ; currm++)
     {
@@ -95,21 +177,83 @@ int main()
         {
             if(currm == 0 || currm == 2 || currm == 4 || currm == 6 || currm == 7 || currm == 9 || currm == 11)
             {
-                printf("\n\nThis month had/has/will have 31 days\n\n");
+                // printf("\n\nThis month had/has/will have 31 days\n\n");
+                days_in_mnth = 31;
             }
 
             else if (currm == 3 || currm == 5 || currm == 8 || currm == 10)
             {
-                printf("\n\nThis month had/has/will have 30 days\n\n");
+                // printf("\n\nThis month had/has/will have 30 days\n\n");
+                days_in_mnth = 30;
             }
 
             else
             {
-                printf("\nThis month had/has/will have 28/29 days\n\n");
+                if (is_leap(year) == 1)
+                {
+                    // printf("\nThis month had/has/will have 29 days\n\n");
+                    days_in_mnth = 29;
+                }
+
+                else
+                {
+                    // printf("\nThis month had/has/will have 28 days\n\n");
+                    days_in_mnth = 28;
+                } 
+                
             }
             
         }
     }
 
+    printf("\n\t\t  %s %d\n\n", month, year);
+
+
+    // print days mon   tue   wed  ...
+    for (int d = 0 ; d < 7 ; d++)
+    {
+        printf("%s\t", days[d]);
+    }
+
+    printf("\n");
+
+    int blanks = 0, date = 1;
+
+    while (blanks < week_day)
+    {
+        printf(" \t");
+        blanks ++;
+    }
+
+    while (date <= days_in_mnth)
+    {
+        if(((week_day) % 7) == 0)
+        {
+            printf("\n\n");
+        }
+
+        printf(" %d\t", date);
+        week_day++;
+        date++;
+    }
+
     return 0;	
+}
+
+
+int is_leap(int yr)
+{
+    int leap;
+    if((yr % 400 == 0) || ((yr % 4 == 0) && (yr % 100 != 0)))
+    {
+        leap = 1;
+        return leap;
+    }
+
+    else
+    {
+        leap = 0;
+        return leap;
+    }
+
 }
